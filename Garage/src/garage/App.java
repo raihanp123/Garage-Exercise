342a2be6178fe97db48cc8733edb93f539e55444
raihanp123 <rainy97@hotmail.co.uk>
2021-07-07 16:39:04 +0100

    adding changes

--- Garage/src/garage/App.java
+++ Garage/src/garage/App.java
@@ -6,9 +6,23 @@ public class App {
 
 		Garage garage = new Garage();
 
-		Vehicle vehicle1 = new Vehicle(1, 4, "Black", "Nissan");
+		Vehicle car1 = new Car(1, 4, "Black", "Nissan", 108);
+		
+		Vehicle motorbike1 = new Motorbike(2, 2, "Red", "Motor", 5);
+		
+		Vehicle bicycle1 = new Bicycle(3, 2, "Blue", "Bikey", 2);
 
+		garage.addVehicle(car1);
+		garage.addVehicle(motorbike1);
+		garage.addVehicle(bicycle1);
+		
 		garage.listVehicles();
+		
+		System.out.println(garage.calculateBill(car1));
+		System.out.println(garage.calculateBill(motorbike1));
+		System.out.println(garage.calculateBill(bicycle1));
+		
+		garage.removeVehicle(car1);
 
 	}
 
--- Garage/src/garage/Garage.java
+++ Garage/src/garage/Garage.java
@@ -6,6 +6,54 @@ public class Garage {
 
 	private ArrayList<Vehicle> vehicles = new ArrayList<>();
 
+	public void listVehicles() {
+
+		for (int i = 0; i < vehicles.size(); i++) {
+
+			System.out.print(vehicles.get(i).getId());
+			System.out.print(vehicles.get(i).getNumberOfWheels());
+			System.out.print(vehicles.get(i).getColour());
+			System.out.print(vehicles.get(i).getMake());
+
+		}
+
+	}
+
+	public float calculateBill(Vehicle vehicle) {
+		
+		 float bill = 100;
+		   
+		 //for (Vehicle v : vehicles) {
+		 if (vehicle instanceof Car) {
+			 bill=  bill * vehicle.getNumberOfWheels();		 	 
+			 } 
+		 else if (vehicle instanceof Motorbike) {
+			 bill=  bill * vehicle.getNumberOfWheels();		 	 
+			 } 
+		 
+		 else if (vehicle instanceof Bicycle) {
+			 bill=  bill / vehicle.getNumberOfWheels();		 	 
+			 } 
+		 
+		 return bill;
+			}
+		 
+	public void removeVehicles(Vehicle vehicle) {
+		
+		if (vehicle instanceof Car) {
+			this.vehicles.remove(vehicle);	 	 
+			 } 
+		}
+	
+	public void print() {
+		System.out.println("Vehicles: [");
+		for (Vehicle v : this.vehicles) {
+			v.print();
+		}
+		System.out.println("]");
+
+	}
+
 	public void addVehicle(Vehicle vehicle) {
 		this.vehicles.add(vehicle);
 	}
@@ -22,28 +70,6 @@ public class Garage {
 	public void setVehicles(ArrayList<Vehicle> vehicles) {
 
 		this.vehicles = vehicles;
-	}
-
-	public void listVehicles() {
-
-		for (int i = 0; i < vehicles.size(); i++) {
-
-			System.out.print(vehicles.get(i).getId());
-			System.out.print(vehicles.get(i).getNumberOfWheels());
-			System.out.print(vehicles.get(i).getColour());
-			System.out.print(vehicles.get(i).getMake());
-
-		}
-
-	}
-
-	public void print() {
-		System.out.println("Vehicles: [");
-		for (Vehicle v : this.vehicles) {
-			v.print();
-		}
-		System.out.println("]");
-
 	}
 
 }
